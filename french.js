6d3410b44990dc6050769bd330d539479863dccb
New Owner <newowner@Dysons-MacBook-Pro.local>
2016-01-28 09:56:29 -0600

    added more French words

--- french.js
+++ french.js
@@ -3,20 +3,26 @@ var Dictionary = (function (prevDictionary) {
 	var frenchLexicon = {
 		"merry":"joyeux", 
 		"christmas":"noël", 
-		"and":"et", 
-		"happy":"content", 
-		"new":"neuf", 
+		"birthday":"anniversaire",	
 		"year":"an",
+		"day":"jour", 
 		"hello":"salut",
 		"thank":"remercier",
 		"you":"toi",
-		"birthday":"anniversaire",
 		"beer":"bière",
-		"is":"est",
-		"good":"bien",
 		"people":"personnes",
-		"are":"sommes",
-		"crazy":"fou"
+		"goodbye": "au revoir",	
+		"crazy":"fou",
+		"new":"neuf", 
+		"good":"bien",
+		"happy":"content", 
+		"best":"meilleur",	
+		"are":"sommes",	
+		"is":"est",
+		"and":"et", 
+		"this":"ce",
+		"the":"la",
+
 	};
 	
 	prevDictionary.getFrenchLexicon = function() {
